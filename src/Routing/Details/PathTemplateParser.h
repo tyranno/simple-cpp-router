a0cbc7b2af3e3c1841c75bacd9c6a4f264ff5b66
tyranno <tyranno1223@gmail.com>
2021-06-05 00:47:28 +0900

    added get match param map

--- src/Routing/Details/PathTemplateParser.h
+++ src/Routing/Details/PathTemplateParser.h
@@ -10,20 +10,21 @@ namespace Routing {
 namespace Details {
 
 class PathTemplateParser {
-public:
-    PathTemplateParser(std::string const &path);
+ public:
+  PathTemplateParser(std::string const &path);
 
-    bool hasDynVars() const;
-    bool hasDynVar(std::string const &name) const;
-    int getDynVarPos(std::string const &name) const;
+  bool hasDynVars() const;
+  bool hasDynVar(std::string const &name) const;
+  int getDynVarPos(std::string const &name) const;
+  const std::map<std::string, int> &getDynVars() const { return _dynVarsPos; }
 
-private:
-    std::map<std::string, int> _dynVarsPos;
-    StringTokenizer _tokenizer;
+ private:
+  std::map<std::string, int> _dynVarsPos;
+  StringTokenizer _tokenizer;
 
-    void parse(std::string const &path);
-    bool validate(std::string const &path);
+  void parse(std::string const &path);
+  bool validate(std::string const &path);
 };
 
-}
-}
+}  // namespace Details
+}  // namespace Routing
